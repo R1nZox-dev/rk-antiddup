d4bf2b5b5628d7724afdab2d09244d90778037e0
R1nZox.16 <150614067+R1nZox-dev@users.noreply.github.com>
2024-05-26 21:31:41 +0200

    Update server.lua

--- rk-antiddup/server.lua
+++ rk-antiddup/server.lua
@@ -1,10 +1,11 @@
 local discord = GetPlayerIdentifierByType(src,'discord')
 local pData = QBCore.Functions.GetPlayer(src)
 local cid = pData.PlayerData.citizenid
-MySQL.scalar('SELECT discord FROM players WHERE citizenid = ?',{cid},function(DID)
-    if DID ~= 'nil' then 
-        if DID ~= discord:sub(9) then 
+MySQL.scalar('SELECT discord FROM players WHERE citizenid = ?',{cid} ,function(Ddata)
+    if Ddata ~= 'nil' then 
+        if Ddata ~= discord:sub(9) then 
             DropPlayer(src, 'DISCORD DUP DETTECTED\nyou can not join the game with this discord ')
+            TriggerEvent("qb-log:server:CreateLog", "anticheat", "Anti DUP discord", "red", "tried to join with this discord : <@"..discord:sub(9) .."> the first discord it's this one <@"..Ddata..">")
         end
     else
         MySQL.update('UPDATE players SET discord = ? WHERE citizenid = ?', {discord:sub(9), cid})
